68d0f639cc4786d3b54db5a02cc2d654fe249104
Oleksandr <porpup@hotmail.com>
2022-12-10 19:52:55 -0500

    small bugs fixed

--- js/actions.js
+++ js/actions.js
@@ -6,7 +6,7 @@ function func1() {
     var num5 = parseInt(document.getElementById("var5").innerHTML);
     var num6 = parseInt(document.getElementById("var6").innerHTML);
     var num7 = parseInt(document.getElementById("var7").innerHTML);
-    var total = num1 + num2 + num3 + num4 + num5 + num6 + num7; 
+    var total = num1 + num2 + num3 + num4 + num5 + num6 + num7;
     alert(num1 + " + " + num2 + " + " + num3 + " + " + num4 + " + " + num5 + " + " + num6 + " + " + num7 + " = " + total);
 }
 
@@ -14,12 +14,12 @@ function func2() {
     var uName = document.getElementById("un").value;
     var pass = document.getElementById("pass").value;
     var pay;
-        if (document.getElementById("visa").checked == true)
-            pay = document.getElementById("visa").value;
-        if (document.getElementById("mc").checked == true)
-            pay = document.getElementById("mc").value;
-        if (document.getElementById("amex").checked == true)
-            pay = document.getElementById("amex").value;
+    if (document.getElementById("visa").checked == true)
+        pay = document.getElementById("visa").value;
+    if (document.getElementById("mc").checked == true)
+        pay = document.getElementById("mc").value;
+    if (document.getElementById("amex").checked == true)
+        pay = document.getElementById("amex").value;
     var month = document.getElementById("month").value;
     var year = document.getElementById("year").value;
     alert("User name: " + uName + "\nPassword: " + pass + "\nPayment: " + pay + "\nExpiration date: " + month + " / " + year);
